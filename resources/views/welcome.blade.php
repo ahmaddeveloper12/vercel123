c8324b3b8b52105f0a9de699d5a498d57b1dbe68
ahmaddeveloper12 <codewithahmad363@gmail.com>
2025-01-09 00:02:27 -0800

    jj

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+dklmlmlmlgml
+dlgmfklsmlmlml
+sdfgkllllllsmfv
+<h2>
+
+    easdfsmlml
+</h2>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo in expedita voluptas excepturi similique. Perferendis placeat quisquam tempora repudiandae consequatur architecto alias corporis ut. Repellat quam ullam voluptatum cupiditate numquam.</p>
